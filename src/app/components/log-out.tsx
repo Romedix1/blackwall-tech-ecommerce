1e16356c475537e3f2543b9dffbe361dc828f067
Romedix1 <doboszmichal4@gmail.com>
2026-02-24 11:34:14 +0100

    feat(a11y): add prefers-reduced-motion support for log out

--- src/app/components/log-out.tsx
+++ src/app/components/log-out.tsx
@@ -1,6 +1,6 @@
 export const LogOutButton = () => {
   return (
-    <button className="text-error-text hover:animate-glitch focus:animate-glitch px-2 text-left uppercase">
+    <button className="text-error-text peefere hover:animate-glitch focus:animate-glitch px-2 text-left uppercase motion-reduce:animate-none motion-reduce:hover:animate-none motion-reduce:focus:animate-none">
       [ sever_connection ]
     </button>
   )
