6a657350fd6ff9a8cbb41da54b9546c89461e20f
Romedix1 <doboszmichal4@gmail.com>
2026-02-25 22:32:54 +0100

    feat: add categories section to main page

--- src/components/layout/navbar/navbar-logo.tsx
+++ src/components/layout/navbar/navbar-logo.tsx
@@ -5,7 +5,7 @@ export const NavbarLogo = () => {
     <Link
       aria-label="Blackwall Store"
       href="/"
-      className="lg:text-20 hover:animate-logo focus:animate-logo hover:text-error-text focus:text-error-text focus-visible:ring-accent focus-visible:ring-offset-background group text-sm font-bold uppercase outline-none focus-visible:ring-2 focus-visible:ring-offset-2 motion-reduce:animate-none motion-reduce:hover:animate-none motion-reduce:focus:animate-none sm:text-lg"
+      className="hover:animate-logo focus:animate-logo hover:text-error-text focus:text-error-text focus-visible:ring-accent focus-visible:ring-offset-background group text-sm font-bold uppercase outline-none focus-visible:ring-2 focus-visible:ring-offset-2 motion-reduce:animate-none motion-reduce:hover:animate-none motion-reduce:focus:animate-none sm:text-lg lg:text-xl"
     >
       <span aria-hidden="true">
         Blackwall_
