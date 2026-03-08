69b1378fb065fdeca048edab3b02bb80e4a7ba7f
Romedix1 <doboszmichal4@gmail.com>
2026-03-08 12:34:31 +0100

    fix: radio input accessibility

--- src/app/(home)/products/[productsCategory]/_components/radio-input.tsx
+++ src/app/(home)/products/[productsCategory]/_components/radio-input.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { InputHTMLAttributes, MouseEvent } from 'react'
+import { InputHTMLAttributes, KeyboardEvent, MouseEvent } from 'react'
 
 interface RadioInputProps extends InputHTMLAttributes<HTMLInputElement> {
   label: string
@@ -8,34 +8,46 @@ interface RadioInputProps extends InputHTMLAttributes<HTMLInputElement> {
 }
 
 export const RadioInput = ({ label, onToggle, ...props }: RadioInputProps) => {
-  const handleRadioClick = (e: MouseEvent<HTMLInputElement>) => {
-    onToggle()
+  const handleKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      e.preventDefault()
+      onToggle()
+    }
   }
 
   return (
     <label className="group flex cursor-pointer items-center gap-x-1 font-mono uppercase">
       <input
-        onClick={handleRadioClick}
+        onToggle={onToggle}
         type="radio"
         onChange={() => {}}
+        onKeyDown={handleKeyDown}
         className="peer sr-only"
         {...props}
       />
 
-      <span className="peer-checked:text-accent" aria-hidden="true">
+      <span
+        className="peer-checked:text-accent peer-focus:text-accent"
+        aria-hidden="true"
+      >
         [
       </span>
 
       <span
-        className="peer-checked:text-accent text-center after:content-['_'] peer-checked:after:content-['x']"
+        className="peer-checked:text-accent peer-focus:text-accent text-center after:content-['_'] peer-checked:after:content-['x']"
         aria-hidden="true"
       ></span>
 
-      <span className="peer-checked:text-accent" aria-hidden="true">
+      <span
+        className="peer-checked:text-accent peer-focus:text-accent"
+        aria-hidden="true"
+      >
         ]
       </span>
 
-      <span className="peer-checked:text-accent ml-1 text-sm">{label}</span>
+      <span className="peer-checked:text-accent peer-focus:text-accent ml-1 text-sm">
+        {label}
+      </span>
     </label>
   )
 }
