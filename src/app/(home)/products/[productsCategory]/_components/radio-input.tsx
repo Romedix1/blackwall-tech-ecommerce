3a49348b8b4f3bb69523fe034679bf7e872d7e2c
Romedix1 <doboszmichal4@gmail.com>
2026-03-08 17:56:02 +0100

    fix: radio input checked on click

--- src/app/(home)/products/[productsCategory]/_components/radio-input.tsx
+++ src/app/(home)/products/[productsCategory]/_components/radio-input.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { InputHTMLAttributes, KeyboardEvent, MouseEvent } from 'react'
+import { InputHTMLAttributes, KeyboardEvent, MouseEvent, useRef } from 'react'
 
 interface RadioInputProps extends InputHTMLAttributes<HTMLInputElement> {
   label: string
@@ -8,18 +8,31 @@ interface RadioInputProps extends InputHTMLAttributes<HTMLInputElement> {
 }
 
 export const RadioInput = ({ label, onToggle, ...props }: RadioInputProps) => {
+  const inputRef = useRef<HTMLInputElement>(null)
+
   const handleKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
-    if (e.key === 'Enter') {
+    if (e.key === 'Enter' || e.key === ' ') {
       e.preventDefault()
       onToggle()
     }
   }
 
+  const handleClick = (e: MouseEvent<HTMLLabelElement>) => {
+    if (e.detail > 0) {
+      e.preventDefault()
+      inputRef.current?.focus()
+      onToggle()
+    }
+  }
+
   return (
-    <label className="group flex cursor-pointer items-center gap-x-1 font-mono uppercase">
+    <label
+      className="group flex cursor-pointer items-center gap-x-1 font-mono uppercase"
+      onClick={handleClick}
+    >
       <input
-        onToggle={onToggle}
         type="radio"
+        ref={inputRef}
         onChange={() => {}}
         onKeyDown={handleKeyDown}
         className="peer sr-only"
