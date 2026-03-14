fd2aad3bfb5747d6647f318ac828094443ff5d0d
Romedix1 <doboszmichal4@gmail.com>
2026-03-14 14:24:43 +0100

    feat: add Link component to product list in categories section

--- src/app/(home)/products/[productsCategory]/_components/product-item.tsx
+++ src/app/(home)/products/[productsCategory]/_components/product-item.tsx
@@ -1,6 +1,7 @@
 import { AddToCartButton } from '@/components/shared/add-to-cart-button'
 import { BackgroundGlow, Button } from '@/components/ui'
 import Image from 'next/image'
+import Link from 'next/link'
 
 type productType = {
   id: string
@@ -18,25 +19,30 @@ export const ProductItem = ({ product, category }: ProductItemProps) => {
   const imageUrl = `https://${process.env.SUPABASE_PROJECT_REF}.supabase.co/storage/v1/object/public/products/${category}/${product.slug}.webp`
 
   return (
-    <article className="flex flex-col gap-6 border p-4">
-      <div className="relative flex aspect-4/3 flex-col items-center justify-center">
-        <Image
-          alt="Product image"
-          src={imageUrl}
-          className="relative z-20 object-contain"
-          width={300}
-          height={300}
-          priority={false}
-        />
-        <BackgroundGlow className="top-[60%] h-24 w-24 blur-[30px]" />
-      </div>
+    <article>
+      <Link
+        href={`/product/${product.slug}`}
+        className="group hover:border-accent flex flex-col gap-6 border p-4"
+      >
+        <div className="relative flex aspect-4/3 flex-col items-center justify-center">
+          <Image
+            alt={`${product.name} image`}
+            src={imageUrl}
+            className="relative z-20 object-contain"
+            width={300}
+            height={300}
+            priority={false}
+          />
+          <BackgroundGlow className="top-[60%] h-24 w-24 blur-[30px] group-hover:scale-200" />
+        </div>
 
-      <div className="flex flex-col gap-1">
-        <h2 className="font-bold uppercase">{product.name}</h2>
-        <p className="text-accent font-bold">$ {product.price.toFixed(2)}</p>
-      </div>
+        <div className="flex flex-col gap-1">
+          <h2 className="font-bold uppercase">{product.name}</h2>
+          <p className="text-accent font-bold">$ {product.price.toFixed(2)}</p>
+        </div>
 
-      <AddToCartButton />
+        <AddToCartButton />
+      </Link>
     </article>
   )
 }
